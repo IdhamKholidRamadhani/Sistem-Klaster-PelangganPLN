ed83e5f6c35fd1fcb859c1757a7d8f16164db458
IdhamKholidRamadhani <dramadan64@gmail.com>
2023-06-21 00:15:37 +0700

    Auth

--- resources/views/content/static/profile.blade.php
+++ resources/views/content/static/profile.blade.php
@@ -13,9 +13,9 @@
                 <div class="flex-auto px-0 pt-0 pb-2">
                     <div class="overflow-x-auto p-7">
 
-                        <h3 class="text-base mb-3">Data Instansi</h3>
+                        <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-3.5 py-1.5 rounded">Data Instansi</span>
                         <form>
-                            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <div class="grid gap-6 mb-6 mt-4 md:grid-cols-2">
                                 <div>
                                     <label for="name"
                                         class="block mb-2 text-sm font-medium text-gray-900">Administrator</label>
@@ -46,22 +46,21 @@
                                 </div>
                             </div>
 
-                            {{-- <h3 class="text-base mt-6 mb-3">Data Akun</h3> --}}
-                            <span class="mt-6 mb-3 bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded">Data Akun</span>
-                            <div class="mb-6">
+                            <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-3.5 py-1.5 rounded">Data Akun</span>
+                            <div class="mt-4 mb-3">
                                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email
                                     address</label>
                                 <input type="email" id="email"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                     value="{{ Auth::user()->email }}" required>
                             </div>
-                            <div class="mb-6">
+                            <div class="mb-3">
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
                                 <input type="password" id="password"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                     placeholder="•••••••••" required>
                             </div>
-                            <div class="mb-6">
+                            <div class="mb-3">
                                 <label for="confirm_password" class="block mb-2 text-sm font-medium text-gray-900">Confirm
                                     password</label>
                                 <input type="password" id="confirm_password"
@@ -69,7 +68,7 @@
                                     placeholder="•••••••••" required>
                             </div>
                             <button type="submit"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Simpan</button>
+                                class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Simpan</button>
                             <a href="/Dashboard" type="button"
                                 class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2">Kembali</a>
                         </form>
